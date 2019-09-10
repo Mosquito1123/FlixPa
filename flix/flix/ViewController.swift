d131ef77250659e217cc54bfb3a1ed92a3b591fd
Carlos Estrada <me@caestrada.com>
2019-09-09 18:24:16 -0700

    Initialize Xcode project

---
+++ flix/flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Carlos Estrada on 9/9/19.
+//  Copyright © 2019 Carlos Estrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
