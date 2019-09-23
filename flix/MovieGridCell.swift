66f907af76b0fcc3e5006d2d3f1d37caea16fee1
Carlos Estrada <me@caestrada.com>
2019-09-22 19:02:46 -0700

    Fetch movies from superhero genre in new tab

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Carlos Estrada on 9/22/19.
+//  Copyright © 2019 Carlos Estrada. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
